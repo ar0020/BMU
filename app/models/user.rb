f03ce3815ec09c649bc415bcd76fae8c4243c021
Martin Cox <neokyxero@gmail.com>
2014-02-19 13:33:46 -0600

    Devise now uses username instead of email
    User.rb was changed to how easy ways of accessing login, and checks
    formatting of username.
    
    Had to add all of the devise views so they could be changed to use login
    instead of email.
    
    init/devise.rb had to be changed to use login
    
    application_controller had to be updated as well.
    
    A migration was made to add username to the table for Users.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,32 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+		 
+	attr_accessor :login
+  
+  def login=(login)
+    @login = login
+  end
+
+  def login
+    @login || self.username || self.email
+  end
+  
+  #### This is the correct method you override with the code above
+  #### def self.find_for_database_authentication(warden_conditions)
+  #### end
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions).first
+    end
+  end
+  
+  validates :username,
+    :uniqueness => {
+      :case_sensitive => false
+    },
+    :format => %r{[a-zA-Z0-9]} # etc.
 end
