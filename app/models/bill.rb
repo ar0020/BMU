ba00fd18ada1b2c6762e827187b39eedc613c719
ar0020 <austinruggles@gmail.com>
2014-03-29 03:56:33 -0500

    Partial for bills

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,2 +1,6 @@
 class Bill < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :account
+  validates :user_id, :account_id, :payee_name, :payee_street, :payee_city, :payee_state, :payee_zip, :payee_account_id, :amount, :pay_date, preseces: true
+
 end
