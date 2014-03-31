ed1d689c59418aa5e7e9cf8eddc7adbec30ae59b
ar0020 <austinruggles@gmail.com>
2014-03-31 05:09:37 -0500

    added function to display account numbers will display formatted account number regardless of size.

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,12 +1,11 @@
 class Account < ActiveRecord::Base
   belongs_to :user
   has_many :transactions
-        #params.require(:account).permit(:user_id, :current_balance, :account_type, :monthly_account_rate, :is_active)
 
   validates :user_id, :account_type, :current_balance, :account_type, :monthly_account_rate, :is_active, presence: true
   before_save :check_if_customer
   before_validation :set_type
-  
+
   attr_accessor :username
 
   TYPES=["Checking","Credit","Market","Mortgage","Saving"]
@@ -21,20 +20,18 @@ class Account < ActiveRecord::Base
     return accounts
   end
 
-  def self.search(query)
-    if query
-      search_condition = "%" + query + "%"
-      find(:all, :conditions => ["account_type LIKE ? OR user_id LIKE ?", search_condition, search_condition])
-    else
-      find(:all)
-    end
-  end
-
   def rate
   end
-  
+
+  def numeric_ending
+    number = "#{self.id}"
+    number = "%.8i" % number
+    last_four = number[number.length - 4, 4]
+    return last_four
+  end
+
   private
-  
+
   def check_if_customer
     user = User.find(self.user_id)
     if user.user_level == 3
@@ -42,8 +39,8 @@ class Account < ActiveRecord::Base
     end
     return false
   end
-  
+
   def set_type
-    
+
   end
 end
