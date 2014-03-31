4af1ff4840581c36bfafd50c88a3e638b8f10e59
Martin Cox <neokyxero@gmail.com>
2014-03-30 20:14:26 -0500

    Bill validation over hall
    created a custom validation for matching things that rails validation
    aren't made for.
    
    Added to check to make sure user_id, account_id, and payee_account_id
    are real.
    
    amount validation is a lot more robust.
    I still don't allow things such as '$' or ',', because I don't want to
    make a string parser.
    Valid Options: 01234.56 or 0.56 or 12345
    Invalid: 0.56789

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -5,26 +5,25 @@ class Bill < ActiveRecord::Base
   validates :user, :account, :payee_name, :payee_street, 
             :payee_city, :payee_state, :payee_zip, :payee_account, 
             :amount, :pay_date, presence: true
-  validates_associated :user, :account
-  validates :amount, numericality: { with: /(?=.)^\$?(([1-9][0-9]{0,2}([0-9]{3})*)|[0-9]+)?(\.[0-9]{1,2})?$/,
-            message: " must not contain commas, and no more than 2 places past a decimal point."}
+  validates_associated :user, :account, :payee_account
+  validates :amount, format: { 
+            with: /^([0-9]+)(.[0-9][0-9])?$/,
+            message: " must not contain commas, and no more than 2 places past a decimal point.",
+            multiline: true}
   validates :payee_zip, length: { is: 5, wrong_length: " should be 5 numbers long."}
   validates :payee_zip, numericality: { only_integer: true }
-  
-  before_save :check_params
+  validate :custom_validation?
 
 
   def bills(offset)
     @bills = Bill.find(offset)
   end
   
-  private
-  
-  def check_params
+  def custom_validation?
     account = Account.find(self.account_id)
-    if account.user_id != self.user_id
-      return false
-    end
-    return true
+    errors.add(:account_id, " must not be from a Mortgage, Market, or Savings account.") if account.account_type != "Checking" && account.account_type != "Credit" 
+    errors.add(:account_id, " must be one of you own.") if account.user_id != self.user_id
+    errors.add(:pay_date, " can't be in the past.") if self.pay_date < Date.yesterday
+    errors.add(:pay_date, " can not be past the 28th of the month.") if self.pay_date.day > 28
   end
 end
