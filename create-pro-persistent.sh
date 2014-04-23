71f675f4e7786f1997040f88d4859f238ec2ccb4
Martin Cox <neokyxero@gmail.com>
2014-04-22 21:50:39 -0500

    some minor graphical patches.

--- create-pro-persistent.sh
+++ create-pro-persistent.sh
@@ -1,1 +1,1 @@
-docker run -d -p 80:3000 -p 5432:5432 -name="bmu-pro" -v /root/workspace/BMU:/home/app/github/BMU neoky/bmu /sbin/init
+docker run -d -p 80:3000 -p 5432:5432 -name="bmu-pro" -v /root/workspace/BMU:/home/app/BMU neoky/bmu /sbin/init
